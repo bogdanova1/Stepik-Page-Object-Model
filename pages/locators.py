e938eaecaf9e5b9fe241bdf466d8fa241d80f7f0
nabog <nabog@onegolabs.com>
2020-10-01 01:00:04 +0300

    add setup

--- pages/locators.py
+++ pages/locators.py
@@ -18,6 +18,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     SHOW_BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
