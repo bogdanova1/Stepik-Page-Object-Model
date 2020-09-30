c3847b9b0a9b8987d52be3f2d6c3cb48d3ec5608
nabog <nabog@onegolabs.com>
2020-10-01 01:36:20 +0300

    add test_screenshots.py

---
+++ conftest.py
@@ -0,0 +1,43 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose UI language for tests")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    if language is None: raise pytest.UsageError("test run should contain language for test")
+
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    browser.maximize_window()
+    browser.implicitly_wait(5)
+
+    yield browser
+
+    print("\nquit browser..")
+    # получаем переменную с текущей датой и временем в формате ГГГГ-ММ-ДД_ЧЧ-ММ-СС
+    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    # делаем скриншот с помощью команды Selenium'а и сохраняем его с именем "screenshot-ГГГГ-ММ-ДД_ЧЧ-ММ-СС"
+    browser.save_screenshot('screenshot-%s.png' % now)
+    browser.quit()
+
